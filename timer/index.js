fbacebe6f6570011bda46af7dc064455f5ec6baa
Vuong Huynh <mannhuynh@gmail.com>
2021-05-11 11:57:04 -0500

    Basic JS implementation for timer app

--- timer/index.js
+++ timer/index.js
@@ -1,1 +1,41 @@
-alert("aaaa")
+
+class Timer{
+    constructor(durationInput, startButton, pauseButton){
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+
+        this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
+    }
+
+    start = () => {
+        this.tick(); 
+        this.interval = setInterval(this.tick, 1000);
+    }
+
+    pause = () => {
+        clearInterval(this.interval);
+    }
+    tick = () => {
+        if (this.getTimeRemain <= 0) {
+            this.pause();
+        } else {
+            this.setTimeRemain = this.getTimeRemain - 1;
+        }
+    }
+
+    get getTimeRemain() {
+        return parseFloat(this.durationInput.value);
+    }
+
+    set setTimeRemain(time) {
+        this.durationInput.value = time;
+    }
+}
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton);
