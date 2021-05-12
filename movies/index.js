8fd9cf8d960ed54674a0f777cdd1de3ed1ff8dee
Vuong Huynh <mannhuynh@gmail.com>
2021-05-12 18:57:05 -0500

    create a reusable autocomplete file

--- movies/index.js
+++ movies/index.js
@@ -1,8 +1,23 @@
-const fetchData = async (searhTerm) => {
-	const response = await axios.get("http://www.omdbapi.com/", {
+createAutoComplete({
+	root: document.querySelector(".autocomplete"),
+	renderOption(movie) {
+		const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
+		return `
+            <img src="${imgSrc}" />
+            ${movie.Title} (${movie.Year})
+        `;
+	},
+	onOptionSelect(movie) {
+		onMovieSelect(movie);
+	},
+    inputValue(movie) {
+        return movie.Title;
+    },
+    async fetchData(searchTerm) {
+        const response = await axios.get("http://www.omdbapi.com/", {
 		params: {
 			apikey: "c3c09d02",
-			s: searhTerm,
+			s: searchTerm,
 		},
 	});
 
@@ -10,61 +25,7 @@ const fetchData = async (searhTerm) => {
 		return [];
 	}
 	return response.data.Search;
-};
-
-const root = document.querySelector(".autocomplete");
-root.innerHTML = `
-    <label for=""><b>Search For a Movie</b></label>
-    <input type="text" class="input">
-    <div class="dropdown">
-    <div class="dropdown-menu">
-        <div class="dropdown-content results"></div>
-    </div>
-    </div>
-`;
-
-const dropdown = document.querySelector(".dropdown");
-const resultsWrapper = document.querySelector(".results");
-const input = document.querySelector("input");
-
-// Fetch data using user's input
-const onInput = async (event) => {
-	const movies = await fetchData(event.target.value);
-
-	if (!movies.length) {
-		dropdown.classList.remove("is-active");
-		return;
-	}
-
-	resultsWrapper.innerHTML = "";
-	dropdown.classList.add("is-active");
-
-	for (let movie of movies) {
-		const option = document.createElement("a");
-		const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
-
-		option.classList.add("dropdown-item");
-		option.innerHTML = `
-            <img src="${imgSrc}" />
-            ${movie.Title}
-        `;
-
-        option.addEventListener("click", () => {
-			dropdown.classList.remove("is-active");
-			input.value = movie.Title;
-            onMovieSelect(movie);
-		});
-		resultsWrapper.appendChild(option);
-	}
-};
-
-// Debouncing user input in search box with 500 ms delay
-input.addEventListener("input", debounce(onInput, 500));
-
-document.addEventListener("click", (event) => {
-	if (!root.contains(event.target)) {
-		dropdown.classList.remove("is-active");
-	}
+    }
 });
 
 const onMovieSelect = async (movie) => {
@@ -74,9 +35,9 @@ const onMovieSelect = async (movie) => {
 			i: movie.imdbID,
 		},
 	});
-    // console.log(movieTemplate(response.data));
+	// console.log(movieTemplate(response.data));
 	// document.querySelector("#summary").innerHTML = movieTemplate(response.data);
-    document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+	document.querySelector("#summary").innerHTML = movieTemplate(response.data);
 };
 
 const movieTemplate = (movieDetail) => {
